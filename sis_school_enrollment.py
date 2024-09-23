5bdd7bbad15c8599ae440582a5b233744ef9adb5
Rizwan Riaz <139876959+rizwanriaz1988@users.noreply.github.com>
2024-09-23 19:08:04 +0500

    Update sis_school_enrollment.py

--- sis_school_enrollment.py
+++ sis_school_enrollment.py
@@ -38,7 +38,7 @@ service = Service(executable_path=chrome_driver_path)
 
 # Set up Chrome options with a random user-agent
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
