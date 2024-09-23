d5994675363147351440db305e46dbfdc1b254dc
Rizwan Riaz <139876959+rizwanriaz1988@users.noreply.github.com>
2024-09-23 15:00:02 +0500

    Update sis_school_enrollment.py

--- sis_school_enrollment.py
+++ sis_school_enrollment.py
@@ -53,8 +53,8 @@ chrome_options.add_argument("--remote-debugging-port=9222")  # Enable remote deb
 chrome = webdriver.Chrome(service=service, options=chrome_options)
 chrome.set_page_load_timeout(30)
 
-# url = 'https://sis.punjab.gov.pk/'
-url = 'https://httpbin.org/ip'
+url = 'https://sis.punjab.gov.pk/'
+# url = 'https://httpbin.org/ip'
 
 max_retries = 3
 
@@ -63,9 +63,9 @@ try:
         try:
             chrome.get(url)
             print("Page loaded successfully.")
-            # Get the displayed IP address
-            ip_address = chrome.find_element(By.TAG_NAME, 'pre').text
-            print("IP Address used:", ip_address)    
+            # # Get the displayed IP address
+            # ip_address = chrome.find_element(By.TAG_NAME, 'pre').text
+            # print("IP Address used:", ip_address)    
             break
         except TimeoutException as e:
             print(f"Attempt {attempt + 1} failed due to timeout. Retrying in 5 seconds...")
@@ -75,100 +75,100 @@ try:
             print(f"WebDriver exception encountered: {e}")
             raise
     
-    # WebDriverWait(chrome, 20).until(EC.element_to_be_clickable((By.ID, "students_search-tab"))).click()
+    WebDriverWait(chrome, 20).until(EC.element_to_be_clickable((By.ID, "students_search-tab"))).click()
     
-    # # Select district and tehsil
-    # district = Select(WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="enrollment_tab_form"]/div[1]/select'))))
-    # district.select_by_value('33')
+    # Select district and tehsil
+    district = Select(WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="enrollment_tab_form"]/div[1]/select'))))
+    district.select_by_value('33')
     
-    # tehsil = Select(WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="enrollment_tab_form"]/div[2]/select'))))
-    # tehsil.select_by_value('116')
+    tehsil = Select(WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="enrollment_tab_form"]/div[2]/select'))))
+    tehsil.select_by_value('116')
 
-    # markaz = Select(WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="enrollment_tab_form"]/div[3]/select'))))
+    markaz = Select(WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="enrollment_tab_form"]/div[3]/select'))))
 
-    # marakaz_list = []
+    marakaz_list = []
 
-    # for markaz_id in range(6449, 6470):
-    #     markaz.select_by_value(str(markaz_id))
-    #     time.sleep(random.uniform(3, 7))  # Random wait between 3 to 7 seconds
+    for markaz_id in range(6449, 6470):
+        markaz.select_by_value(str(markaz_id))
+        time.sleep(random.uniform(3, 7))  # Random wait between 3 to 7 seconds
 
-    #     filter_button_xpath = '/html/body/div[5]/div/div/div/div/div/div/div[3]/div[12]/div[3]/div/div/div/div[1]/div/form/div[7]/button'
-    #     WebDriverWait(chrome, 20).until(EC.element_to_be_clickable((By.XPATH, filter_button_xpath))).click()
+        filter_button_xpath = '/html/body/div[5]/div/div/div/div/div/div/div[3]/div[12]/div[3]/div/div/div/div[1]/div/form/div[7]/button'
+        WebDriverWait(chrome, 20).until(EC.element_to_be_clickable((By.XPATH, filter_button_xpath))).click()
 
-    #     time.sleep(random.uniform(3, 7))  # Random wait after clicking the filter
+        time.sleep(random.uniform(3, 7))  # Random wait after clicking the filter
 
-    #     parent_element_path = "((//*[name()='svg'])[1]//*[name()='g' and @class='highcharts-series-group']//*[name()='g'])[1]"
-    #     parent_element = WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, parent_element_path)))
+        parent_element_path = "((//*[name()='svg'])[1]//*[name()='g' and @class='highcharts-series-group']//*[name()='g'])[1]"
+        parent_element = WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, parent_element_path)))
 
-    #     rect_elements = parent_element.find_elements(By.TAG_NAME, "rect")
-    #     print(len(rect_elements))
+        rect_elements = parent_element.find_elements(By.TAG_NAME, "rect")
+        print(len(rect_elements))
 
-    #     actions = ActionChains(chrome)
+        actions = ActionChains(chrome)
 
-    #     for bar in rect_elements:
-    #         actions.move_to_element(bar).perform()
-    #         time.sleep(random.uniform(1, 3))  # Random wait for tooltip to appear
+        for bar in rect_elements:
+            actions.move_to_element(bar).perform()
+            time.sleep(random.uniform(1, 3))  # Random wait for tooltip to appear
 
-    #         school_emis_xpath = "//*[name()='svg']//*[name()='g' and @class='highcharts-label highcharts-tooltip highcharts-color-undefined']//*[name()='text']//*[name()='tspan'][1]"
-    #         school_total_enroll_xpath = "//*[name()='svg']//*[name()='g' and @class='highcharts-label highcharts-tooltip highcharts-color-undefined']//*[name()='text']//*[name()='tspan'][3]"
+            school_emis_xpath = "//*[name()='svg']//*[name()='g' and @class='highcharts-label highcharts-tooltip highcharts-color-undefined']//*[name()='text']//*[name()='tspan'][1]"
+            school_total_enroll_xpath = "//*[name()='svg']//*[name()='g' and @class='highcharts-label highcharts-tooltip highcharts-color-undefined']//*[name()='text']//*[name()='tspan'][3]"
 
-    #         try:
-    #             school_emis_element = WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, school_emis_xpath)))
-    #             school_total_enroll_element = WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, school_total_enroll_xpath)))
+            try:
+                school_emis_element = WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, school_emis_xpath)))
+                school_total_enroll_element = WebDriverWait(chrome, 20).until(EC.presence_of_element_located((By.XPATH, school_total_enroll_xpath)))
 
-    #             school_emis = school_emis_element.text
-    #             school_total_enroll = school_total_enroll_element.text
-    #             school_enroll = school_total_enroll[7:]
+                school_emis = school_emis_element.text
+                school_total_enroll = school_total_enroll_element.text
+                school_enroll = school_total_enroll[7:]
 
-    #             marakaz_list.append({
-    #                 'EMIS Code': school_emis,
-    #                 'Enrollment': school_enroll
-    #             })
+                marakaz_list.append({
+                    'EMIS Code': school_emis,
+                    'Enrollment': school_enroll
+                })
 
-    #         except Exception as e:
-    #             print(f"An error occurred: {e}")
-    #             raise
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                raise
 
-    #     print(len(marakaz_list))
+        print(len(marakaz_list))
 
-    # df = pd.DataFrame(marakaz_list)
+    df = pd.DataFrame(marakaz_list)
 
-    # current_date = datetime.now().strftime("%d-%m-%Y")
-    # excel_filename = f"school_wise_enrollment_data_{current_date}.xlsx"
-    # df.to_excel(excel_filename, index=False, engine='openpyxl')
-    # print(f"Data saved to {excel_filename}")
+    current_date = datetime.now().strftime("%d-%m-%Y")
+    excel_filename = f"school_wise_enrollment_data_{current_date}.xlsx"
+    df.to_excel(excel_filename, index=False, engine='openpyxl')
+    print(f"Data saved to {excel_filename}")
 
-    # # Google Sheets API integration
-    # SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-    # credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    # sheets_service = build('sheets', 'v4', credentials=credentials)
+    # Google Sheets API integration
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    sheets_service = build('sheets', 'v4', credentials=credentials)
 
-    # SPREADSHEET_ID = '1i0KG-we9EqZt-PeAPxYscKpVb60sfpq-OcVISJz3a7g'
-    # RANGE_NAME = 'Sheet1!A1'
+    SPREADSHEET_ID = '1i0KG-we9EqZt-PeAPxYscKpVb60sfpq-OcVISJz3a7g'
+    RANGE_NAME = 'Sheet1!A1'
 
-    # df = pd.read_excel(excel_filename)
-    # df.fillna('', inplace=True)
-    # data = df.values.tolist()
-    # header = df.columns.tolist()
-    # values = [header] + data
+    df = pd.read_excel(excel_filename)
+    df.fillna('', inplace=True)
+    data = df.values.tolist()
+    header = df.columns.tolist()
+    values = [header] + data
 
-    # body = {
-    #     'values': values
-    # }
+    body = {
+        'values': values
+    }
 
-    # sheets_service.spreadsheets().values().clear(
-    #     spreadsheetId=SPREADSHEET_ID,
-    #     range=RANGE_NAME
-    # ).execute()
+    sheets_service.spreadsheets().values().clear(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME
+    ).execute()
 
-    # result = sheets_service.spreadsheets().values().update(
-    #     spreadsheetId=SPREADSHEET_ID,
-    #     range=RANGE_NAME,
-    #     valueInputOption='RAW',
-    #     body=body
-    # ).execute()
+    result = sheets_service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME,
+        valueInputOption='RAW',
+        body=body
+    ).execute()
 
-    # print(f"{result.get('updatedCells')} cells updated in Google Sheets.")
+    print(f"{result.get('updatedCells')} cells updated in Google Sheets.")
 
 except Exception as e:
     print(f"An error occurred: {e}")
